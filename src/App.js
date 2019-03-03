27abb27308cffa765131ce59013c745181a29486
Burak Tilek <burak.tilek@gmail.com>
2019-03-03 23:23:43 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss'
 
-class App extends Component {
+class Book extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <section className='book_container'>
+      </section>
+    )
   }
 }
 
-export default App;
+
+class App extends Component {
+  state = {
+    books: [],
+    query: ''
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
+        <Book />
+      </div>
+    )
+  }
+}
+
+export default App
