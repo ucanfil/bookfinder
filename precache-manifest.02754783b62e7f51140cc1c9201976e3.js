4e915ed4ca5d0df69879d6efbd9b386bfdbdcd9d
Burak Tilek <burak.tilek@gmail.com>
2019-03-04 14:09:58 +0300

    Updates

---
+++ precache-manifest.02754783b62e7f51140cc1c9201976e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e63a2a12b477cd274d6",
+    "url": "/bookfinder/static/css/main.90fc5c8c.chunk.css"
+  },
+  {
+    "revision": "5e63a2a12b477cd274d6",
+    "url": "/bookfinder/static/js/main.5e63a2a1.chunk.js"
+  },
+  {
+    "revision": "ea93145390ff8b0cfc07",
+    "url": "/bookfinder/static/js/runtime~main.ea931453.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/bookfinder/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "4119431e9f83244ce0699b87bd6213e1",
+    "url": "/bookfinder/index.html"
+  }
+];
